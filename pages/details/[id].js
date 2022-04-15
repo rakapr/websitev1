71dd415064c0aad4c158a11d6d3b115017e12f4d
rakapr <94906708+rakapr@users.noreply.github.com>
2022-04-15 15:27:42 +0500

    Update [id].js

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -147,7 +147,7 @@ export default function Post({ postData }) {
 }
 
 export async function getStaticPaths() {
-  const paths = ["/details/1", "/details/2"];
+  const paths = ["/details/[]", "/details/[]"];
   return { paths, fallback: true };
 }
 
