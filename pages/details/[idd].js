5fc7e9c339280f9db6c3f10224884b77b0d9ae43
Syed Hassam Kazmi <59208947+Hassamkazmi@users.noreply.github.com>
2022-04-18 12:48:39 +0500

    Update [idd].js

--- pages/details/[idd].js
+++ pages/details/[idd].js
@@ -147,7 +147,7 @@ export default function Post({ postData }) {
 }
 
 export async function getStaticPaths() {
-  const paths = ["/details/[sku]", "/details/[sku]"];
+  const paths = ["/details/sku", "/details/sku"];
   return { paths, fallback: true };
 }
 
