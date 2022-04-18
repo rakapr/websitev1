6eb4aa76f4f68e898f292250953a13712755e776
Syed Hassam Kazmi <59208947+Hassamkazmi@users.noreply.github.com>
2022-04-18 10:41:57 +0500

    Update [id].js

--- pages/catagory/[id].js
+++ pages/catagory/[id].js
@@ -133,7 +133,7 @@ export default function Post({ postData }) {
 }
 
 export async function getStaticPaths() {
-  const paths = ["/catagory/[id]", "/catagory/[id]"];
+  const paths = ["/catagory/[idd]", "/catagory/[idd]"];
   return { paths, fallback: true };
 }
 
